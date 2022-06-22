51174a968d46f39b97e3f8f8cdae410ac2f4252d
Vishnuram Hariharan <vinu4794@gmail.com>
2022-06-22 13:16:17 -0400

    Added swagger & lambda function files

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps('Application under development. Search functionality will be implemented in Assignment 3')
+    }
